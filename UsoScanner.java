9be6995a347d79ff5296c6a62a0c5b5bb57834f2
yegecali <yegecali2@gmail.com>
2022-03-04 15:03:52 -0500

    uso Scanner

---
+++ UsoScanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UsoScanner {
+    public static void main (String [] args){
+        int n1, n2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el Primer numero:");
+        n1 = sc.nextInt();
+        System.out.println("Ingrese el Segundo numero:");
+        n2 = sc.nextInt();
+        System.out.println("El resultado de sumar: " + n1 + " + " + n2 + " = " + (n1+ n2));
+    }
+}
